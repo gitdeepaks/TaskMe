d0b0723f223429b2b5d9a43dd47ea9bd198c0ee0
gitdeepaks <deepaksankhyan92@outlook.com>
2023-12-07 21:39:52 +0530

    added prisma db 🚀

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,22 @@
+"use client";
+
 import React from "react";
 
 const OrganizationIdPage = () => {
-  return <div>Organization Page</div>;
+  console.log("I am looged in the browser");
+  return (
+    <div>
+      <form>
+        <input
+          id="title"
+          name="title"
+          required
+          placeholder="Enter a border title"
+          className="border-black border p-1"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;

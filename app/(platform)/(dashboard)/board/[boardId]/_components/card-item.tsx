c024ec242aea38ff7d6c304c1282647ad392bd6c
gitdeepaks <deepaksankhyan92@outlook.com>
2023-12-12 15:17:47 +0530

    tring to remove the drag and drop bug 🚀

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -16,7 +16,8 @@ const CardItem = ({ data, index }: CardItemProps) => {
           {...provided.dragHandleProps}
           ref={provided.innerRef}
           role="button"
-          className="truncate boder-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
+          onClick={() => {}}
+          className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
         >
           {data.title}
         </div>

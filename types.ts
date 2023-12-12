ba92466f2c88195af63a3fe8be544bb069cfa3f0
gitdeepaks <deepaksankhyan92@outlook.com>
2023-12-12 20:28:54 +0530

    added card modals 🚀

--- types.ts
+++ types.ts
@@ -1,4 +1,4 @@
 import { Card, List } from "@prisma/client";
 
 export type ListWithCards = List & { cards: Card[] };
-export type CardWithList = Card & { lists: List };
+export type CardWithList = Card & { list: List };

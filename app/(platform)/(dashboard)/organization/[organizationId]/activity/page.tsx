1b389218676e1398861da8c5521466b4bc789bad
gitdeepaks <deepaksankhyan92@outlook.com>
2023-12-14 19:28:43 +0530

    prop bug fix 🚀

--- app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
@@ -1,12 +1,17 @@
-import React, { Suspense } from "react";
-import Info from "../_components/info";
-import { Separator } from "@/components/ui/separator";
-import { ActivityList } from "./_components/activity-list";
+import { Suspense } from "react";
 
-const ActivityPage = () => {
+import { Separator } from "@/components/ui/separator";
+
+import { ActivityList } from "./_components/activity-list";
+import { checkSubscription } from "@/lib/subscription";
+import Info from "../_components/info";
+
+const ActivityPage = async () => {
+  const isPro = await checkSubscription();
+
   return (
-    <div className="-w-full">
-      <Info />
+    <div className="w-full">
+      <Info isPro={isPro} />
       <Separator className="my-2" />
       <Suspense fallback={<ActivityList.Skeleton />}>
         <ActivityList />
